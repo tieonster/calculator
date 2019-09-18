50d9689898c919886395ce087c06fbbed55338a9
loichiilek <loichiilek@gmail.com>
2019-09-18 14:28:47 +0800

    file name and comments

---
+++ calculator/easy.py
@@ -0,0 +1,27 @@
+def addition (arg1, arg2):
+  #========================================#
+  # input: integer arg1, integer arg2      #
+  # return: integer/ string                #
+  #========================================#
+  return 0
+
+def substraction (arg1, arg2):
+  #========================================#
+  # input: integer arg1, integer arg2      #
+  # return: integer/ string                #
+  #========================================#
+  return 0
+
+def multiplication (arg1, arg2):
+  #========================================#
+  # input: integer arg1, integer arg2      #
+  # return: integer/ string                #
+  #========================================#
+  return 0
+
+def division (arg1, arg2):
+  #========================================#
+  # input: integer arg1, integer arg2      #
+  # return: integer/ string                #
+  #========================================#
+  return 0
