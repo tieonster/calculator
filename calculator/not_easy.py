74dd9d6f2f983661e231250360733739403a40d9
loichiilek <loichiilek@gmail.com>
2019-09-18 15:34:40 +0800

    added 2 new functions

---
+++ calculator/not_easy.py
@@ -0,0 +1,20 @@
+# function checks if arg1 is a prime number
+def isPrime(arg1):
+  #========================================#
+  # input: integer arg1                    #
+  # return: boolean or string              #
+  # eg. input: 13 => return: True          #
+  # eg. input: 12 => return: False         #
+  #========================================#
+  return False
+
+# function checks if arg1 is a palindrome
+def isPalindrome(arg1):
+  #========================================#
+  # input: integer arg1                    #
+  # return: boolean or string              #
+  # eg. input: 12321 => return: True       #
+  # eg. input: 86155168 => return: True    #
+  # eg. input: 1234567 => return: False    #
+  #========================================#
+  return False
